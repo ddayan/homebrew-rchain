aa954a35363274dfb2019c4a80858c48ce1be0ae
Dror Dayan <dayandror@gmail.com>
2018-12-12 12:06:39 +0800

    Update to rnode 0.8.1, and update Java dependency to v10+

--- rnode.rb
+++ rnode.rb
@@ -1,20 +1,18 @@
 class Rnode < Formula
   desc "Offical implementation of the RChain platform."
   homepage "developer.rchain.coop"
-  version "0.7.1"
+  version "0.8.1"
   url "https://github.com/rchain/rchain/releases/download/v#{version}/rnode-#{version}.tgz"
-  sha256 "82becb03e2cd183eee87f7b71edb2fc68c766546dab0b72daafd4503248889f4"
+  sha256 "df67b49d5c977eb94fcc027a6e63f969e1c68a59952daa556dc861e59709dd19"
 
-  JAVA_VERSION = "1.8"
-
-  depends_on :java => JAVA_VERSION
+  depends_on :java => "10+"
   depends_on "libsodium"
 
   def install
     rm_f Dir["bin/*.bat"]
     libexec.mkpath
     libexec.install %w[bin lib]
-    (bin/"rnode").write_env_script "#{libexec}/bin/rnode", "JAVA_HOME" => "$(#{Language::Java.java_home_cmd(JAVA_VERSION)})"
+    bin.write_exec_script "#{libexec}/bin/rnode"
   end
 
   test do
